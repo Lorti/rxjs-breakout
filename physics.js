380990090d5abed8d88f1dceb5723d7b7421a94c
Manuel Wieser <office@manuelwieser.com>
2016-03-28 14:46:55 +0200

    clean all the things

--- physics.js
+++
@@ -1,6 +0,0 @@
-export function detectCollision(circle, rectangle) {
-    return circle.x + circle.radius > rectangle.x
-        && circle.x - circle.radius < rectangle.x + rectangle.width
-        && circle.y + circle.radius > rectangle.y
-        && circle.y - circle.radius < rectangle.y + rectangle.height;
-}
